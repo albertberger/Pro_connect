6f8912db8cb5b6c3530dec6906b6f0ea539267c3
albertberger <albertberger125@gmail.com>
2023-10-06 16:45:03 -0300

    homepage

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('meusProjetos').addEventListener('click', function() {
+    alert('Cliquei em Meus Projetos!');
+  });
+  
+  document.getElementById('testeCompetencia').addEventListener('click', function() {
+    alert('Cliquei em Teste de Competência!');
+  });
